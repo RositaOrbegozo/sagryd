9dd54a6a3a9803754c2232ee2a0ecb33198e7d28
RositaOrbegozo <51015950+RositaOrbegozo@users.noreply.github.com>
2022-07-15 21:26:42 -0500

    Update Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -1,7 +1,7 @@
 <?php
-    const BASE_URL = "http://sagryd-prevaed-prueba.cu.ma/Pextumuy/";
+    const BASE_URL = "http://app-sagryd.herokuapp.com";
     const HOST = "localhost";
-    const BD = "pextumuy";
+    const BD = "pextumuy"
     const DB_USER = "root";
     const PASS = "root";
     const CHARSET = "charset=utf8";
