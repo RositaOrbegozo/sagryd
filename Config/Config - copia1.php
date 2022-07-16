262756727d7f734c8faeb5bcaf05940b910e331d
RositaOrbegozo <51015950+RositaOrbegozo@users.noreply.github.com>
2022-07-15 21:27:05 -0500

    Delete Config - copia1.php

--- Config/Config - copia1.php
+++
@@ -1,8 +0,0 @@
-<?php
-    const BASE_URL = "http://sagryd-prevaed-prueba.cu.ma/";
-    const HOST = "sagryd-prevaed-prueba.cu.ma";
-    const BD = "sagrydpr_sagrydBD";
-    const DB_USER = "sagrydpr_root";
-    const PASS = "Rosit@22OD";
-    const CHARSET = "charset=utf8";
-?>
